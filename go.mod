9efd53b509ad70487ccb414399935ae969ee7b39
Sher-mad <80169323+Sher-mad@users.noreply.github.com>
2021-04-25 01:40:58 +0500

    Начальная версия

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sher-mad/bank_Lection_11
+
+go 1.16
---
+++ pkg/bank/types/types.go
@@ -0,0 +1,29 @@
+package types
+
+// Money представляет собой в минимальных единицах (центы, копейки, дирам и т.д. )
+type Money int64
+
+// Currency представляет код валют
+type Currency string
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN  представляет номер карты
+type PAN string
+
+// Card  представляет информацию о платёжной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
